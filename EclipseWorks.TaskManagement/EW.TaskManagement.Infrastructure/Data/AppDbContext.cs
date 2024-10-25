7bc55ed8e1b7fd4564c85bb29d02f6be6af5c6fa
LucasSousaAmaral <lucasdovah@gmail.com>
2024-10-25 05:17:33 -0300

    Adding Database context

---
+++ EclipseWorks.TaskManagement/EW.TaskManagement.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using EW.TaskManagement.Domain.Entities;
+using EW.TaskManagement.Infrastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace EW.TaskManagement.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<TaskItem> TaskItems { get; set; }
+    public DbSet<TaskHistory> TaskHistories { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new ProjectConfiguration());
+        modelBuilder.ApplyConfiguration(new TaskItemConfiguration());
+        modelBuilder.ApplyConfiguration(new TaskHistoryConfiguration());
+        modelBuilder.ApplyConfiguration(new CommentConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}

6f6439928166049a76769c44b972c48ef74e8f2d
LucasSousaAmaral <lucasdovah@gmail.com>
2024-10-25 05:19:22 -0300

    General API setup

--- EclipseWorks.TaskManagement/EclipseWorks.TaskManagement/Program.cs
+++ EclipseWorks.TaskManagement/EclipseWorks.TaskManagement/Program.cs
@@ -1,4 +1,33 @@
+using EW.TaskManagement.Application.Interfaces;
+using EW.TaskManagement.Application.Services;
+using EW.TaskManagement.Application.Servicess;
+using EW.TaskManagement.Domain.Repositories;
+using EW.TaskManagement.Infrastructure.Data;
+using EW.TaskManagement.Infrastructure.Repositories;
+using EW.TaskManagement.Presentation.Mappings;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurar o DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+/// Injeção de dependência
+
+// Mapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+//Repositories
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+builder.Services.AddScoped<ITaskItemRepository, TaskItemRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+// Services
+builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<ITaskItemService, TaskItemService>();
+builder.Services.AddScoped<IPerformanceReportService, PerformanceReportService>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 // Add services to the container.
 
@@ -14,9 +43,14 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
-
-app.UseHttpsRedirection();
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
 
